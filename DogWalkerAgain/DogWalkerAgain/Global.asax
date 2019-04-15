6d243da0b961588e293879613f32072fa5953f84
Sarah Steiner <steiner.sa96@gmail.com>
2019-04-15 14:53:36 -0500

    New project

---
+++ DogWalkerAgain/DogWalkerAgain/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DogWalkerAgain.MvcApplication" Language="C#" %>
---
+++ DogWalkerAgain/DogWalkerAgain/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DogWalkerAgain.Startup))]
+namespace DogWalkerAgain
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
