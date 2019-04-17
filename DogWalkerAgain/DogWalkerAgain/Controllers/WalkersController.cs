8888039f5b4d15a7e940f2393fea3dec8d17daa5
samsmllr <sam.s.mllr@gmail.com>
2019-04-17 14:26:05 -0500

    Cleaned up walkerController method dogsearch and recreated dogsearch view; the combination of both now accurately allows for dogs to be filtered by name and breed or some combination of the two.

--- DogWalkerAgain/DogWalkerAgain/Controllers/WalkersController.cs
+++ DogWalkerAgain/DogWalkerAgain/Controllers/WalkersController.cs
@@ -32,12 +32,17 @@ namespace DogWalker.Controllers
         }
 
         //Search dogs
-        public async Task<ActionResult> DogSearch(string dogBreed, string searchString)
+        public ActionResult DogSearch(string dogBreed, string searchString)
         {
+            var BreedList = new List<string>();
+
             //use LINQ to get list of dog breeds.
             IQueryable<string> BreedGet = from d in db.Dogs
                                           orderby d.Breed
                                           select d.Breed;
+
+            BreedList.AddRange(BreedGet.Distinct());
+            ViewBag.dogBreed = new SelectList(BreedList);
 
             var dogs = from d in db.Dogs
                        select d;
@@ -53,13 +58,12 @@ namespace DogWalker.Controllers
                 dogs = dogs.Where(d => d.Breed == dogBreed);
             }
 
-            var dogBreedViewModel = new DogBreedViewModel
-            {
-                Breeds = new SelectList(await BreedGet.Distinct().ToListAsync()),
-                Dogs = await dogs.ToListAsync()
-            };
+            //{
+            //    Breeds = new SelectList(await BreedGet.Distinct().ToListAsync()),
+            //    Dogs = await dogs.ToListAsync()
+            //};
 
-            return View(dogBreedViewModel);
+            return View(dogs);
         }
 
         //GET: Details of dog picked
